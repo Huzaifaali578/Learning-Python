f5501e575be846cee62867a93f68e8e6ec271872
Huzaifa Nagariya <huzaifaali578@gmail.com>
2026-05-02 16:01:04 +0530

    First Program in python.

---
+++ firstProgram.py
@@ -0,0 +1,25 @@
+#write a program to add two numbers
+# a = int(input("Enter first number:"))
+# b = int(input("Enter second number:"))
+# sum = a+b
+# print("sum=",sum)
+
+#write a program to find the area of a square
+# side = int(input("Enter the side of a squre: "))
+# area = side ** 2
+# print("Area=",area)
+
+#WAP to input two floating number nd find its average
+# a = float(input("Enter the first float: "))
+# b = float(input("Enter the second float: "))
+# avg = (a+b)/2
+# print("average:",avg)
+
+#WAP to input two int numbers, a and b
+#print True if a is greater then b.input
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+if(a>b):
+    print(True)
+else:
+    print(False)
