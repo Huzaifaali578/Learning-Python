9a2a2efbf2d825971b4fd5dc7625c751cb38ff0f
Huzaifa Nagariya <huzaifaali578@gmail.com>
2026-05-03 17:26:21 +0530

    completed Lecture 4.

---
+++ Lecture 3/lecture_three.py
@@ -0,0 +1,25 @@
+#WAP to ask the user to enter the name of their 3 favourite movies and store them in a list
+# movies = []
+# movie1 = input("enter mvie name 1: ")
+# movie2 = input("enter mvie name 2: ")
+# movie3 = input("enter mvie name 3: ")
+# movies.append(movie1)
+# movies.append(movie2)
+# movies.append(movie3)
+# print(movies)
+
+#WAP to chcek if list contains palidrome of elements.
+# list = [1, 2, 3, 2, 6]
+# listCopy = list.copy()
+# listCopy.reverse()
+# if(list == listCopy):
+#     print("List is palindrome")
+# else:
+#     print("List is not a palindrome")
+
+#WAP to count the no. of student with "A" grade in the following tupple.
+tup = ("A", "B", "C", "D", "E", "A", "B", "A")
+print(tup.count("A"))
+list = ["A", "B", "C", "D", "E", "A", "B", "A"]
+list.sort()
+print(list)
