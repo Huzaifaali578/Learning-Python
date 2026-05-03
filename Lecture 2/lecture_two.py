db69d5257c528c1886e481f709617e2b48b6b243
Huzaifa Nagariya <huzaifaali578@gmail.com>
2026-05-03 16:28:45 +0530

    Lecture 2 completed.

---
+++ Lecture 2/lecture_two.py
@@ -0,0 +1,39 @@
+# WAP to input user name and prnt in lenght.
+# str = input("Enter your name: ")
+# print(len(str))
+
+# WAP to input string and find the occurence of $ in string
+# str = input("Enter string which containce $: ")
+# print(str.count("$"))
+
+#WAP to check if a number enter by the user is odd and even 
+# number = int(input("Enter the number: "))
+# rem = number % 2
+# if(rem == 0):
+#     print("even")
+# else:
+#     print("odd")
+
+#WAP to find the greatest of 3 number enter by the user
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+# num4 = int(input("Enter fourth number: "))
+
+# if (num1 > num2 and num1 > num3 and num1 > num4):
+#     print(num1)
+# elif (num2 > num3 and num2 > num4):
+#     print(num2)
+# elif(num3 > num4):
+#     print(num3)
+# else:
+#     print(num4)
+
+#WAP to check if number is multiple of 7 or not
+a = int(input("Enter number: "))
+rem = a % 7
+
+if(rem == 0):
+    print("This number is multple of 7")
+else:
+    print("This is not multiple of 7")
